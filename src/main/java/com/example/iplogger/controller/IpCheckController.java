d9dfa7a37c17d14139af4d742c6c1fa51e5bd5d1
spat!alM0use <sbw@bk.ru>
2022-03-21 23:35:07 +0400

    Выделен сервис для получения инфы по IP (пока без дифференциации) TODO: продумать разделение провайдеров

--- src/main/java/com/example/iplogger/controller/IpCheckController.java
+++ src/main/java/com/example/iplogger/controller/IpCheckController.java
@@ -2,47 +2,47 @@ package com.example.iplogger.controller;
 
 import com.example.iplogger.domain.IpData;
 import com.example.iplogger.domain.IpRequest;
-import com.example.iplogger.domain.providers.ipWhoIsProvider.IpWhoIsInfo;
+import com.example.iplogger.service.IpInfoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * на вход приходит IP, на выходе - данные от провайдера. Сейчас реализован ipwhois.
- * TODO: выделить сбор инфо по ip в отдельный сервис
  * TODO: сделать  интеграцию ipregistry.co (он точнее)
  * TODO: обеспечить модульность и переключаемость источника
  * TODO: сделать реализацию внутреннего хранилища ???
+ * TODO: может кэш?
  */
 
+@Slf4j
 @RestController
 @RequestMapping("/ip-check")
 public class IpCheckController {
-    private static final String PROVIDER_URL = "http://ipwhois.app/json/";
-    private final RestTemplate template;
     private final PagingAndSortingRepository<IpData, String> repo;
+    private final IpInfoService ipInfoService;
 
     @GetMapping
     public IpData getInfoByIp(@RequestBody IpRequest request) {
-        String urlToApi = PROVIDER_URL.concat(request.getIp());
-        ResponseEntity<IpWhoIsInfo> entity = template.getForEntity(urlToApi, IpWhoIsInfo.class);
-        IpWhoIsInfo body = entity.getBody();
-        if(body == null){
-            throw new IllegalArgumentException("Inappropriate data received from geoIP provider");
-        }
-        //todo: IpWhoIsInfo -> IpData
-//        return repo.save(body);
-        return null;
+        log.info("Recieved request for IP: {}", request);
+        IpData ipData = ipInfoService.getInfoByIp(request.getIp());
+        repo.save(ipData);
+        return ipData;
+    }
+
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "/me")
+        public IpData getOwnIpData(HttpServletRequest req){
+        String remoteAddr = req.getRemoteAddr();
+        IpData infoByIp = ipInfoService.getInfoByIp(remoteAddr);
+        return repo.save(infoByIp);
     }
 
     @Autowired
-    public IpCheckController(RestTemplate template, PagingAndSortingRepository<IpData, String> repo) {
-        this.template = template;
+    public IpCheckController(IpInfoService ipInfoService, PagingAndSortingRepository<IpData, String> repo) {
+        this.ipInfoService = ipInfoService;
         this.repo = repo;
     }
 }
---
+++ src/main/java/com/example/iplogger/service/IpInfoService.java
@@ -0,0 +1,35 @@
+package com.example.iplogger.service;
+
+import com.example.iplogger.domain.IpData;
+import com.example.iplogger.domain.providers.ipWhoIsProvider.IpWhoIsInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+
+@Service
+public class IpInfoService {
+    private final RestTemplate template;
+    @Value("${application.provider.url}")
+    private String providerUrl;
+
+    public IpData getInfoByIp(String targetAddress) {
+        String urlToApi = providerUrl.concat(targetAddress);
+        ResponseEntity<IpWhoIsInfo> entity = template.getForEntity(urlToApi, IpWhoIsInfo.class);
+        IpWhoIsInfo body = entity.getBody();
+        if(body == null){
+            throw new IllegalArgumentException("Inappropriate data received from geoIP provider");
+        }
+        //todo: IpWhoIsInfo -> IpData
+        // как лучше реализовать multisource - через прототип или заводить разных провайдеров, и выбирать нужный...
+        return null;
+    }
+
+
+    @Autowired
+    public IpInfoService(RestTemplate template) {
+        this.template = template;
+    }
+}

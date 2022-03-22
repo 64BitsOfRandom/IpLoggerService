b4d2fd6b3b6933e990893f587cc277fdf5ea800a
spat!alM0use <sbw@bk.ru>
2022-03-22 22:35:21 +0400

    Добавлено сохранение запросов ip

--- src/main/java/com/example/iplogger/domain/rest/ipchecker/IpResponse.java
+++ src/main/java/com/example/iplogger/domain/rest/ipchecker/IpResponse.java
@@ -1,20 +1,28 @@
-package com.example.iplogger.domain.ip;
+package com.example.iplogger.domain.rest.ipchecker;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
-@Data
+import javax.persistence.Embeddable;
+import javax.persistence.Embedded;
+
+@Getter
+@Setter
 @Builder
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
 public class IpResponse {
     private String browser;
     private String city;
+    @Embedded
     private Coordinates coordinates;
     private String country;
     private String ip;
     private String isp;
 
     @Data
+    @Embeddable
+    @NoArgsConstructor
     @AllArgsConstructor
     public static class Coordinates{
         private double latitude;
